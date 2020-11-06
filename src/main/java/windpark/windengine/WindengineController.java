75063adf285eab0e697b2267d071ac33b4af1845
dkopcinski <dawid.kopcinski2003@gmail.com>
2020-11-06 10:55:21 +0100

    Controller and pom.xml updated

--- src/main/java/windpark/windengine/WindengineController.java
+++ src/main/java/windpark/windengine/WindengineController.java
@@ -13,25 +13,25 @@ public class WindengineController {
 
     @Autowired
     private WindengineService service;
-	
+
     @RequestMapping("/")
     public String windengineMain() {
-    	String mainPage = "This is the windengine application! (DEZSYS_GK72_WINDPARK) <br/><br/>" +
-                          "<a href='http://localhost:8080/windengine/001/data'>Link to windengine/001/data</a><br/>" +
-                          "<a href='http://localhost:8080/windengine/001/transfer'>Link to windengine/001/transfer</a><br/>";
+        String mainPage = "This is the windengine application! (DEZSYS_GK72_WINDPARK) <br/><br/>" +
+                "<a href='http://localhost:8080/windengine/001/xml'>Link to windengine/001/xml</a><br/>" +
+                "<a href='http://localhost:8080/windengine/001/json'>Link to windengine/001/json</a><br/>" +
+                "<a href='consumer.html'>Link to consumer</a><br/>";
         return mainPage;
     }
-    
-    @RequestMapping("/windengine/{windengineID}/data")
-    public WindengineData windengineData( @PathVariable String windengineID ) {
+
+    @RequestMapping(value="/windengine/{windengineID}/xml", produces={"application/xml"})
+    public WindengineData windengineXml( @PathVariable String windengineID ) {
         return service.getWindengineData( windengineID );
     }
 
-    @RequestMapping("/windengine/{windengineID}/transfer")
-    public String windengineTransfer( @PathVariable String windengineID ) {
-        return service.getGreetings("Windengine.Transfer!");
+    @RequestMapping(value="/windengine/{windengineID}/json", produces={"application/json"})
+    public WindengineData windengineJson( @PathVariable String windengineID ) {
+        return service.getWindengineData( windengineID );
     }
 
 
-    
 }
